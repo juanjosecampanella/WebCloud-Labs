d237c2cc0294568a6b85157537406cbf65f905b3
Juan José Campanella De Oro <juancampanelladeoro@gmail.com>
2025-09-04 22:12:17 -0500

    Footer changes

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -9,8 +9,7 @@ function Footer() {
         <p className="footer-text">© 2024 WebCloud Labs. All rights reserved.</p>
 
         <ul className="social-links">
-          <li><a href="#"><FaFacebookF /></a></li>
-          <li><a href="#"><FaInstagram /></a></li>
+          <li><a href="https://www.instagram.com/webcloudlabs"><FaInstagram /></a></li>
           <li><a href="https://www.linkedin.com/company/webcloudlabs"><FaLinkedinIn /></a></li>
         </ul>
       </div>
