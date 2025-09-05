3ae43efa74da4369527591eccb6ba4672fe500ca
Juan José Campanella De Oro <juancampanelladeoro@gmail.com>
2025-09-04 23:26:48 -0500

    Photos Updates

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -5,12 +5,12 @@ import "../CSS/Service.css";
 function servicesInfo() {
   const Info = [
     {
-      image: "/assets/softwareDevelopment.PNG",
+      image: "/public/assets/softwareDevelopment.PNG",
       serviceTitle: "Software Development",
       description: "We develop software applications that will boost your business.",
     },
     {
-      image: "/assets/dataScience.PNG",
+      image: "/public/assets/dataScience.PNG",
       serviceTitle: "Data Science",
       description: "We analyze the information provided and make reports based on the data for a better approach to your business.",
     },
